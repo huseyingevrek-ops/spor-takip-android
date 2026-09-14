c3d47061915f64cd131701d35bb813a5aea4ca8c
huseyingevrek-ops <huseyingevrek@googlemail.com>
2026-09-14 08:32:09 +0200

    Bump app version to 5.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.i4ae.sportakip"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "4.0.0"
+        versionCode = 5
+        versionName = "5.0.0"
     }
 
     buildTypes {
